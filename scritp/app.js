e721007ea7db6a11a4020b883fe995f847b25c32
MuhammadAbbas-coder004 <mabbasmshahi2003@gmail.com>
2025-11-05 18:48:56 +0500

    issue fix

--- scritp/app.js
+++ scritp/app.js
@@ -9,8 +9,7 @@ import {
   getDocs,
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
-// const userSection = document.querySelector(".user-section");
-const userImg = document.querySelector(".userImg");
+
 const loginBtn = document.querySelector("#loginBtn");
 const logoutBtn = document.querySelector("#logoutBtn");
 const uploadBtn = document.querySelector(".uploadBtn");
@@ -23,7 +22,7 @@ onAuthStateChanged(auth, (user) => {
 
     logoutBtn.style.display = "block"
     loginBtn.style.display = "none";
-    uploadBtn.style.display = "block";
+   
 
     getUserProfile(uid);
     getUserListings(uid); // 👈 only user-specific products
